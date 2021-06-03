013751bacf867ecdeda8c5378011915d9f8cdeeb
Aaron Smith <smitham50@gmail.com>
2021-06-03 17:19:59 -0400

    set up structure for search filters

--- reducers/trailReducer.js
+++ reducers/trailReducer.js
@@ -1,5 +1,12 @@
+import { 
+  filterByDistance, 
+  filterByLength, 
+  filterByDifficulty 
+} from '../utils/searchFilters'; 
+
 const defaultState = {
-  trails: []
+  trails: [],
+  displayedTrails: []
 }
 
 export default function TrailReducer(prevState = defaultState, action) {
@@ -7,7 +14,23 @@ export default function TrailReducer(prevState = defaultState, action) {
     case "SET_TRAILS":
       return {
         ...prevState,
-        trails: action.payload
+        trails: action.payload,
+        displayedTrails: action.payload
+      }
+    case "FILTER_BY_DISTANCE":
+      return {
+        ...prevState,
+        displayedTrails: filterByDistance([...prevState.trails])
+      }
+    case "FILTER_BY_LENGTH":
+      return {
+        ...prevState,
+        displayedTrails: filterByLength([...prevState.trails])
+      }
+    case "FILTER_BY_DIFFICULTY":
+      return {
+        ...prevState,
+        displayedTrails: filterByDifficulty([...prevState.trails])
       }
     default: {
       return prevState
---
+++ utils/searchFilters.js
@@ -0,0 +1,12 @@
+// still need a way to calculate distance
+export function filterByDistance(trails) {
+  return trails;
+}
+
+export function filterByLength(trails) {
+  return trails;
+}
+
+export function filterByDifficulty(trails) {
+  return trails;
+}
